61e74835d483437cfbf870d97c44bdb775096c4f
krishnakumarkari <s556489@nwmissouri.edu>
2023-01-24 12:04:24 -0600

    Added New Repository

---
+++ OOS/src/Unit1/Hello.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Unit1;
+
+/**
+ *
+ * @author krishnakumark
+ */
+public class Hello {
+    
+    public static void main(String[] arg){
+        
+        System.out.println("Hello");
+        System.out.print("oos class");
+    }
+ 
+  }
